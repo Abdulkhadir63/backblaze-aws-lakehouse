bc606d7e96a86c85c0f8a3960759f89db71eac39
abdul khadir <abdulkhadir63633@gmail.com>
2026-09-21 15:04:37 +0530

    Add GitHub Actions Glue deployment permissions

---
+++ terraform/environments/dev/iam-github-glue.tf
@@ -0,0 +1,44 @@
+resource "aws_iam_role_policy" "github_actions_glue" {
+  name = "GitHubActionsGlueDeployment"
+  role = aws_iam_role.github_actions_artifact.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid    = "ManageGlueArtifacts"
+        Effect = "Allow"
+
+        Action = [
+          "s3:PutObject",
+          "s3:AbortMultipartUpload",
+          "s3:GetObject",
+          "s3:GetObjectVersion"
+        ]
+
+        Resource = "${aws_s3_bucket.deployment_artifacts.arn}/glue/*"
+      },
+      {
+        Sid    = "DeployGlueJobs"
+        Effect = "Allow"
+
+        Action = [
+          "glue:GetJob",
+          "glue:UpdateJob"
+        ]
+
+        Resource = [
+          "arn:aws:glue:ap-south-1:131912110087:job/bronze_ingestion",
+          "arn:aws:glue:ap-south-1:131912110087:job/silver_cleaned",
+          "arn:aws:glue:ap-south-1:131912110087:job/data_quality_check",
+          "arn:aws:glue:ap-south-1:131912110087:job/gold_layer",
+          "arn:aws:glue:ap-south-1:131912110087:job/bronze_layer",
+          "arn:aws:glue:ap-south-1:131912110087:job/silver_layer",
+          "arn:aws:glue:ap-south-1:131912110087:job/data_quality_layer",
+          "arn:aws:glue:ap-south-1:131912110087:job/gold_analytics_layer"
+        ]
+      }
+    ]
+  })
+}
