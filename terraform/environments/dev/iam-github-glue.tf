f988cf4db92686a1bb05a46088c726360d729c38
abdul khadir <abdulkhadir63633@gmail.com>
2026-09-21 15:25:06 +0530

    Allow GitHub Actions to pass Glue execution role

--- terraform/environments/dev/iam-github-glue.tf
+++ terraform/environments/dev/iam-github-glue.tf
@@ -38,6 +38,22 @@ resource "aws_iam_role_policy" "github_actions_glue" {
           "arn:aws:glue:ap-south-1:131912110087:job/data_quality_layer",
           "arn:aws:glue:ap-south-1:131912110087:job/gold_analytics_layer"
         ]
+      },
+      {
+        Sid    = "PassGlueExecutionRole"
+        Effect = "Allow"
+
+        Action = [
+          "iam:PassRole"
+        ]
+
+        Resource = "arn:aws:iam::131912110087:role/Glue-S3-Access-Role"
+
+        Condition = {
+          StringEquals = {
+            "iam:PassedToService" = "glue.amazonaws.com"
+          }
+        }
       }
     ]
   })
